9a28527ba04c2fda942f4c3dc6ada98054750f20
Valentina Garavaglia <vaahxx@gmail.com>
2022-11-07 20:43:43 -0500

    structure and header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Content } from "./styles";
+
+const logoImg: string = require("../../assets/logo.svg").default;
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  );
+}
