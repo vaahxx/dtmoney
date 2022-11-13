f4f6a056e9be0d8f42defd15e6dda78d18a11481
Valentina Garavaglia <vaahxx@gmail.com>
2022-11-13 14:07:57 -0500

    getting table data dynamically

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 
 createServer({
-  routes() {
-    this.namespace = "api";
-    this.get("/transactions", () => {
-      return [
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: "Transaction 1",
-          amount: 400,
+          title: "Freelance de website",
           type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
+          category: "Desenvolvimento",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
         },
-      ];
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdrawal",
+          category: "Casa",
+          amount: 1000,
+          createdAt: new Date("2021-02-13 09:00:00"),
+        },
+      ],
+    });
+  },
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
+
+    this.post("transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
     });
   },
 });
